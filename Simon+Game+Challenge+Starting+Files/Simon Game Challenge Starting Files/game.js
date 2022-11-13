d102016c223dba5665a50575c4e76400dec823c7
nishant32400 <67227861+nishant32400@users.noreply.github.com>
2022-11-13 23:32:49 +0530

    Add files via upload

---
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,40 @@
+var buttonColours = ['red','blue','green','yellow'];
+var gamePattern = [];
+var userClickedPattern = [];
+
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    // Flashing a random button
+    
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    //sound of random button
+    playSound(randomChosenColour);
+    animatePress();
+}
+
+
+$('.btn').click(function(){
+    var userChosenColour = $(this).attr('id');
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+})
+
+
+function playSound(name){
+        var audio = new Audio('sounds/'+ name + '.mp3');
+        audio.play(); 
+}
+
+
+function animatePress(currentColour){
+    $('#'+ currentColour).addClass('pressed');
+    setTimeout(function(){
+        $('#'+ currentColour).removeClass('pressed');
+    },100);
+}
+
